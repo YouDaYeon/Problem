d581f6679390fad9f933e28b6de18564f494bf6d
USER <USER@LAPTOP-AOQJ8K8V>
2023-03-05 15:24:53 +0900

    기초-입출력

--- src/codeUp/sec02/P18.java
+++ src/codeUp/sec02/P18.java
@@ -3,12 +3,14 @@ package codeUp.sec02;
 import java.util.Scanner;
 
 //시간 입력받아 그대로 출력하기
+//시(hour)와 분(minute)이 ":"으로 구분
 public class P18 {
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		String time = sc.nextLine();
-		System.out.println(time);
+		String []b = time.split(":");
+		System.out.println(b[0]+":"+b[1]);
 	}
 
 }
