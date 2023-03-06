c53c83d4280349af118c6dce39261e83ca89efe4
USER <USER@LAPTOP-AOQJ8K8V>
2023-03-06 21:59:49 +0900

    기초-데이터형

---
+++ src/codeUp/sec03/P28.java
@@ -0,0 +1,14 @@
+package codeUp.sec03;
+
+import java.util.Scanner;
+
+public class P28 {
+//정수 1개 입력받아 그대로 출력 (long)
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		long num = sc.nextLong();
+		
+		System.out.println(num);
+	}
+
+}
---
+++ src/codeUp/sec03/P29.java
@@ -0,0 +1,15 @@
+package codeUp.sec03;
+
+import java.util.Scanner;
+
+public class P29 {
+//실수 1개를 입력받아 그대로 출력(double)
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		double i = sc.nextDouble();
+		
+		System.out.printf("%.11f",i);
+		
+	}
+
+}
