25e890ac8707d01d9da4ae959cf2c32ef40aa367
USER <USER@LAPTOP-AOQJ8K8V>
2023-03-30 17:41:03 +0900

    k번째 수

--- src/programmers/KthNumber.java
+++ src/programmers/KthNumber.java
@@ -3,52 +3,48 @@ package programmers;
 import java.util.Arrays;
 
 //k번째 수
-class Solution {
-	public int[] solution(int[]array, int[][] commands) {
+
+//class Solution4 {
+//    public int[] solution(int[] array, int[][] commands) {
+//        int[] answer = new int[commands.length];
+//        int num_length=0;
+//        
+//        for(int i=0;i<commands.length;i++) {
+//        	num_length = commands[i][1]-commands[i][0]+1;
+//        	int[] num = new int[num_length];
+//        	int k=0;
+//        	for(int j=commands[i][0]-1;j<commands[i][1];j++) {
+//        		num[k]=array[j];
+//        		k++;
+//        	}
+//        	Arrays.sort(num);
+//        	answer[i]=num[commands[i][2]-1];
+//        }
+//        return answer;
+//    }
+//}
+
+//Arrays.copyOfRange를 사용
+class Solution4 {
+	public int[] solution(int[] array, int[][] commands) {
 		int[] answer = new int[commands.length];
-		for(int i=0; i<commands.length; i++) {
-			int[] newArray = Arrays.copyOfRange(array, commands[i][0]-1, commands[i][1]);
-			Arrays.sort(newArray);
-			answer[i] = newArray[commands[i][2]-1];
+		
+		for(int i=0;i<commands.length;i++) {
+			int[] temp = Arrays.copyOfRange(array, commands[i][0]-1, commands[i][1]);
+			Arrays.sort(temp);
+			answer[i]=temp[commands[i][2]-1];
 		}
 		return answer;
 	}
 }
-//class Solution {
-//    public int[] solution(int[] array, int[][] commands) {
-//    	
-//    	int[] answer = new int[commands.length];
-//    	for(int i=0; i<commands.length; i++) {
-//    		answer[i] = findKthNum(array, commands[i][0],commands[i][1],commands[i][2]);
-//    	}
-//    	return answer;
-//    }
-//    
-//    int findKthNum(int[] array, int i, int k, int j) {
-//    	int kthNum = 0;
-//    	int[] newArray = new int[k-i+1];
-//    	int index=0;
-//    	for(int t=i-1; t<k; t++) {
-//    		newArray[index]=array[t];
-//    		index++;
-//    	}
-//    	Arrays.sort(newArray);
-//    	kthNum = newArray[j-1];
-//    	
-//    	return kthNum;
-//    }
-//}
+
 public class KthNumber {
-
+	
 	public static void main(String[] args) {
-		int[] array = {1, 5, 2, 6, 3, 7, 4};
-		int[][] commands = {{2, 5, 3}, {4, 4, 1}, {1, 7, 3}};
-		Solution sol = new Solution();
+		Solution4 sol = new Solution4();
+		int[] array = {1,5,2,6,3,7,4};
+		int[][] commands = {{2,5,3},{4,4,1},{1,7,3}};
 		
-		int[] answer = new int[commands.length];
-		answer = sol.solution(array, commands);
-		System.out.println(Arrays.toString(answer));
-		
+		System.out.println(Arrays.toString(sol.solution(array, commands)));
 	}
-
 }
