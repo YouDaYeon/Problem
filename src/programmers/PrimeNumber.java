8c57ecb2147c1124d6a9aa91dba8002777d725e0
USER <USER@LAPTOP-AOQJ8K8V>
2023-04-03 20:24:57 +0900

    소수 만들기

--- src/programmers/PrimeNumber.java
+++ src/programmers/PrimeNumber.java
@@ -1,42 +1,43 @@
 package programmers;
 
-import java.util.ArrayList;
-
 //소수 만들기
-class Solution {
-	public int solution(int[] nums)
-	{
-		//3개의 수 더하기
-		int sosuNum = 0;
-		for(int i=0; i<nums.length-2; i++) {
-			for(int j=i+1; j<nums.length-1; j++) {
-				for(int k=j+1; k<nums.length; k++) {
-					int num = nums[i]+nums[j]+nums[k];
-					if(check_sosu(num)==true) sosuNum++;  
-				}
-			}
-		}
-		return sosuNum;
-	}
-	//소수체크
-	public boolean check_sosu(int n)
-	{
-		boolean check = true;
-		for(int i=2; i<n; i++) {
-			if(n%i==0) {
-				check = false;
-				break;
-			}
-		}
+class Solution5 {
+    public int solution(int[] nums) {
+        int answer = 0;
+
+        int sum=0;
+        for(int i=0;i<nums.length;i++) {
+        	for(int j=i+1;j<nums.length;j++) {
+        		for(int k=j+1;k<nums.length;k++) {
+        			sum=nums[i]+nums[j]+nums[k];
+        			if(check_sosu(sum)==true) answer++;
+        		}
+        	}
+        }
+        return answer;
+    }
+    //소수인지 판단
+    //num이 소수일 시 true값 반환
+    public boolean check_sosu(int num) {
+    	boolean check = true;												
+    	
+    	for(int i=2;i<num;i++) {
+    		if(num%i==0) {
+    			check=false;
+    			break;
+    		}
+    	}
 		return check;
-	}
+    }
 }
+
 public class PrimeNumber {
 
 	public static void main(String[] args) {
-		int[] nums = {1, 2, 3, 4};
+//		int[] nums = {1, 2, 3, 4};
+		int[] nums = {1, 2, 7, 6, 4};
 		
-		Solution sol = new Solution();
+		Solution5 sol = new Solution5();
 		int answer = sol.solution(nums);
 		
 		System.out.println(answer);
